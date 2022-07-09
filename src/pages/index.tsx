75c663b5ee9bfb57b4a871780398b6e6ec52a9c7
Spet77 <76452706+Yelloo5191@users.noreply.github.com>
2022-07-08 18:43:27 -0700

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Heading, VStack } from "@chakra-ui/react"
+
+const Home: NextPage = () => {
+  return (
+    <VStack>
+      <Heading color="black">Boilerplate</Heading>
+    </VStack>
+  )
+}
+
+export default Home
